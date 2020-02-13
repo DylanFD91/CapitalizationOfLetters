cd90ab0541cbcc0f1f51f3edae878efee0836bea
Dylan <dylan.f.derouchie@gmail.com>
2020-02-13 08:14:57 -0600

    init

---
+++ CapitalizeLetters/CapitalizingLetters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalizeLetters
+{
+    class CapitalizingLetters
+    {
+        /*
+        //member variables
+        string userInput;
+        string textInputted;
+        char capitalizing;
+        //constructor
+        //methods
+        public void GetUserInput()
+        {
+            Console.WriteLine("enter in some text");
+            userInput = Console.ReadLine();
+        }
+        public void SplitWordsUp()
+        {
+            textInputted = userInput.Split(' ');
+        }
+        public void CapitalizeIndividualWords()
+        {
+            char[] capitalizing = textInputted.ToCharArray();
+            capitalizing.ToString();
+        }
+
+
+
+
+
+        public void RunProgram()
+        {
+            GetUserInput();
+
+        }
+        */
+    }
+}
---
+++ CapitalizeLetters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalizeLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CapitalizingLetters cpt = new CapitalizingLetters();
+            //cpt.GetUserInput();
+
+
+            string userInput = Console.ReadLine();
+            string result = "";
+            char[] theTextInput = userInput.ToCharArray();
+            string capitalizing = theTextInput[0].ToString();
+            string firstLetterCapital = capitalizing.ToUpper();
+
+            result += firstLetterCapital;
+            for (int index = 1; index < userInput.Length; index++)
+            {
+                if (userInput[index] == ' ')
+                {
+                    if (char.IsLower(userInput[index + 1]))
+                    {
+                        index++;
+                        result += (" " + char.ToUpper(userInput[index]));
+                    }
+                }
+                else
+                {
+                    result += userInput[index];
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
